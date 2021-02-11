abd058b794faed2a6a6d8cd74b0db2ab740aae1d
hassan kiwan <hassanhitleap1@gmail.com>
2021-02-11 16:04:46 +0200

    commit

---
+++ controllers/SendMailController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\controllers;
+
+class SendMailController extends \yii\web\Controller
+{
+
+
+    /**
+     * Run SomeModel::some_method for a period of time
+     * @param string $from
+     * @param string $to
+     * @return int exit code
+     */
+    public function actionInit($from, $to){
+        $dates  = CronJob::getDateRange($from, $to);
+        $command = CronJob::run($this->id, $this->action->id, 0, CronJob::countDateRange($dates));
+        if ($command === false){
+            return Controller::EXIT_CODE_ERROR;
+        }else{
+            foreach ($dates as $date) {
+                //this is the function to execute for each day
+                SomeModel::some_method((string) $date);
+            }
+            $command->finish();
+            return Controller::EXIT_CODE_NORMAL;
+        }
+    }
+    /**
+     * Run SomeModel::some_method for today only as the default action
+     * @return int exit code
+     */
+    public function actionIndex(){
+        return $this->actionInit(date("Y-m-d"), date("Y-m-d"));
+        return $this->render('index');
+    }
+    /**
+     * Run SomeModel::some_method for yesterday
+     * @return int exit code
+     */
+    public function actionYesterday(){
+        return $this->actionInit(date("Y-m-d", strtotime("-1 days")), date("Y-m-d", strtotime("-1 days")));
+    }
+
+}
